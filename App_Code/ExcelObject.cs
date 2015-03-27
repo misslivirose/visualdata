8ec17f9c1992565093933816746013ba85150961
Livi <livi@outlook.com>
2015-03-26 21:32:54 -0700

    Added ExcelObject class to create JSON objects later

---
+++ App_Code/ExcelObject.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Excel;
+
+/// <summary>
+/// Summary description for ExcelObject
+/// </summary>
+public class ExcelObject
+{
+    public string[] columns {get; set;}
+    public List<Row> rows { get; set; }
+
+	public ExcelObject(int _numColumns, int _numRows){
+		columns = new string [_numColumns];
+        rows = new List<Row>();
+	}
+    /**
+     * Insert a row from the Excel spreadsheet into its ExcelObject type.
+     * Each row will be a separate entity to visualize.
+     **/
+    public int InsertRow(Row _rowToAdd){
+        if (_rowToAdd != null) {
+            rows.Add(_rowToAdd);
+            return 0;
+        }
+        return -1;
+    }
+}
--- Visualizer.aspx.cs
+++ Visualizer.aspx.cs
@@ -4,34 +4,50 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Script.Serialization;
 using Excel;
 
 
 public partial class Visualizer : System.Web.UI.Page
 {
-    protected void Page_Load(object sender, EventArgs e)
-    {
+    protected void Page_Load(object sender, EventArgs e){
         String path = @"C:\Users\livieric\Documents\GitHub Projects\visualdata\Resources\sampledata.xlsx";
-        foreach (var worksheet in Workbook.Worksheets(path))
-        {
-            //We have the rows for each segment
-            foreach (var row in worksheet.Rows)
-            {
-                //Get the cells in every row
-                foreach (var cell in row.Cells)
-                {
-                    //Make sure the cell has content
-                    if(cell != null)
-                    {
-                        //Do stuff - format a JSON object for all of the data in a spreadsheet? 
-                        Label newLabel = new Label();
-                        newLabel.Text = cell.Text + " ";
-                        form1.Controls.Add(newLabel);
-                    }
-                }
-                //Visual break between rows
-                form1.Controls.Add(new LiteralControl("<br>"));
+        foreach (var worksheet in Workbook.Worksheets(path)) {
+            int numberOfColumns = worksheet.NumberOfColumns;
+            int numberOfRows = worksheet.Rows.Length;
+
+            ExcelObject _graph = new ExcelObject(numberOfColumns, numberOfRows);
+            /**
+             * First, we take the first row and separate out our column headers
+             * Assume that the workbook data is formatted properly
+             * First row is name, each row after that is a specific object
+             **/
+            var _firstRow = worksheet.Rows.First<Row>();
+            for (int i = 0; i < worksheet.NumberOfColumns; i ++ ){
+                _graph.columns[i] = _firstRow.Cells[i].Text;
             }
+            /**
+             *  Next, we take the rest of the rows and feed them into specific
+             *  Excel objects, each one representing an element in the graph.
+             **/
+            for (int j = 1; j < numberOfRows; j++ ) {
+                _graph.InsertRow(worksheet.Rows[j]);
+            }
+                //We have the rows for each segment
+                foreach (var row in worksheet.Rows){
+                    //Get the cells in every row
+                    foreach (var cell in row.Cells){
+                        //Make sure the cell has content
+                        if (cell != null){
+                            //Do stuff - format a JSON object for all of the data in a spreadsheet? 
+                            Label newLabel = new Label();
+                            newLabel.Text = cell.Text + " ";
+                            form1.Controls.Add(newLabel);
+                        }
+                    }
+                    //Visual break between rows
+                    form1.Controls.Add(new LiteralControl("<br>"));
+                }
         }    
     }
 }
