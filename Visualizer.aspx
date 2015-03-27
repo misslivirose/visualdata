5ac68fec3796ec50ec20205862dfb7d65542bf2e
Livi <livi@outlook.com>
2015-03-26 17:53:43 -0700

    reading from local Excel sheet and programatically adding labels with cell content

---
+++ Visualizer.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Visualizer.aspx.cs" Inherits="Visualizer" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h1> Welcome to the Excel Digital Visualizer </h1>
+    </div>
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ Visualizer.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Excel;
+
+
+public partial class Visualizer : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        String path = @"C:\Users\livieric\Documents\GitHub Projects\visualdata\Resources\sampledata.xlsx";
+        foreach (var worksheet in Workbook.Worksheets(path))
+        {
+            foreach (var row in worksheet.Rows)
+            {
+                foreach (var cell in row.Cells)
+                {
+                    if(cell != null)
+                    {
+                        Label newLabel = new Label();
+                        newLabel.Text = cell.Text;
+                        form1.Controls.Add(newLabel);
+                    }
+                }
+            }
+        }    
+    }
+}
