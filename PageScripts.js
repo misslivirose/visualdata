b75c998de4a017f1bb126aae3f56318ff59b6c5b
Livi <livi@outlook.com>
2015-04-01 16:15:21 -0700

    Updated JS script to render canvas

--- PageScripts.js
+++ PageScripts.js
@@ -4,6 +4,7 @@
 }
 
 function PaintCanvas() {
+
     var renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setPixelRatio(window.devicePixelRatio);
     document.body.appendChild(renderer.domElement);
@@ -12,6 +13,12 @@ function PaintCanvas() {
 
     // Create a three.js camera
     var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.3, 10000);
+
+    var effect = new THREE.VREffect(renderer);
+    effect.setSize(window.innerWidth, window.innerHeight);
+
+    var vrmgr = new WebVRManager(effect);
+
     // Create 3d objects
     var geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
     var material = new THREE.MeshNormalMaterial();
@@ -21,5 +28,9 @@ function PaintCanvas() {
     // Add cube mesh to your three.js scene
     scene.add(cube);
 
-    alert("Canvas Function!");
+    if (vrmgr.isVRMode()) {
+        effect.render(scene, camera);
+    } else {
+        renderer.render(scene, camera);
+    }
 }
