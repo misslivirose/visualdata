1ec4f9278c7689dfc4e19162c5d2a1c30f04f69c
Livi <livi@outlook.com>
2015-04-07 12:05:55 -0700

    Fixed animation loop

--- Scripts/PageScripts.js
+++ Scripts/PageScripts.js
@@ -1,17 +1,20 @@
 ﻿function alertFromJSON(string) {
 
-  
+
 }
 
 function PaintCanvas(string) {
     var renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setPixelRatio(window.devicePixelRatio);
-    document.body.appendChild(renderer.domElement);
+
+    var canvas = renderer.domElement;
+    document.body.appendChild(canvas);
 
     var scene = new THREE.Scene();
 
     // Create a three.js camera
     var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.3, 10000);
+    var controls = new THREE.VRControls(camera);
 
     var effect = new THREE.VREffect(renderer);
     effect.setSize(window.innerWidth, window.innerHeight);
@@ -19,39 +22,45 @@ function PaintCanvas(string) {
     var vrmgr = new WebVRManager(effect);
 
     // Create 3d objects based on the passed in data
-
-    var numObjects = numberOfCubes(string);
-        // Position cube mesh
-        for (var i = 0; i < numObjects; i++)
-        {
-            var geometry = new THREE.BoxGeometry(1, 1, 1);
-            var material = new THREE.MeshNormalMaterial();
-            var cube = new THREE.Mesh(geometry, material);
-            cube.position.z = -10;
-            cube.position.x = i*2;
-            // Add cube mesh to your three.js scene
-            scene.add(cube);
-        }
+    // Get the number of objects we have
+    var obj = $.parseJSON(string);
+    var numObjects = numberOfCubes(obj);
+    for (var i = 0; i < numObjects; i++) { 
+        createCube(i, obj, scene);   
+    }
 
     if (vrmgr.isVRMode()) {
         effect.render(scene, camera);
     } else {
         renderer.render(scene, camera);
     }
-
+    function animate() {
+        // Update VR headset position and apply to camera.
+        controls.update();
+        // Render the scene through the VREffect, but only if it's in VR mode.
+        if (vrmgr.isVRMode()) {
+            effect.render(scene, camera);
+        } else {
+            renderer.render(scene, camera);
+        }
+        requestAnimationFrame(animate);
+    }
+    animate();
 }
 
-function numberOfCubes(string)
-{                                                                                                                          
-    var obj = $.parseJSON(string);
+function numberOfCubes(obj) {
     return obj.rows.length;
+}
 
-    /** Note: Keep for notes on iterating through JSON
-    var data_set = obj.rows[0];
-
-    $.each($.parseJSON(string), function (key, value) {
-        count++;
-    });
-    
-    return count; **/
+function createCube(size, obj, scene)
+{
+    var i = size;
+    var data_set = obj.rows[i];
+    var geometry = new THREE.BoxGeometry(1, data_set.price * .01, 1);
+    var material = new THREE.MeshNormalMaterial();
+    var cube = new THREE.Mesh(geometry, material);
+    cube.position.z = -10;
+    cube.position.x = i * 2;
+    // Add cube mesh to your three.js scene
+    scene.add(cube);
 }
